392fe1c80bcfe92fcfa35a940d7c50b39e7ac784
Parker Lewis <parker.lewis14@att.net>
2015-09-09 18:06:59 -0700

    deleted controller so student login now possible

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  	mount_devise_token_auth_for 'User', at: 'api/auth', controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+  	mount_devise_token_auth_for 'User', at: 'api/auth'
+  	# , controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   	root 'statics#index'
     # get "/omniauth/:provider/callback" => 'sessions#create'
