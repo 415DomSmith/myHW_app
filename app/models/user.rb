a6efe6188307e06c73cf02be99e9ca27aa2ca313
Parker Lewis <parker.lewis14@att.net>
2015-09-09 17:43:19 -0700

    added omniauth info for google in omniauth.rb and updated model to be onmiauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :confirmable, :omniauthable, :omniauth_providers => [:google_oauth2]
   include DeviseTokenAuth::Concerns::User
 end
