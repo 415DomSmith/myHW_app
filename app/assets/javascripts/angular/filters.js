32058e246ff2c30b1b9fa489220ad01546846dea
Parker Lewis <parker.lewis14@att.net>
2015-09-21 15:08:54 -0700

    can query by names and switching to branch so I can work on search by category

--- app/assets/javascripts/angular/filters.js
+++ app/assets/javascripts/angular/filters.js
@@ -26,3 +26,11 @@ app.filter('grade', function () {
    		}
    };
  });
+
+// app.filter('capitalize', function() {
+//   return function(input, scope) {
+//     if (input)
+//     input = input.toLowerCase();
+//     return input.substring(0,1).toUpperCase()+input.substring(1);
+//   }
+// });
