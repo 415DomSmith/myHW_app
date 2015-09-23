9b8801df1036bb7b816756630527c05672fde362
415DomSmith <415.dominic.smith@gmail.com>
2015-09-23 14:40:16 -0700

    fixing deploy

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module MyHWApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+      rewrite %r{^(?!.*(api|auth|omniauth|\.)).*$}, '/'
+    end
   end
 end
